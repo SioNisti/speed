3d45cf6e0b27f1d520cbecf1cc922d6d72cf041b
SioNisti <38298141+SioNisti@users.noreply.github.com>
2021-02-23 20:11:11 +0200

    Create category.cs

---
+++ game/category.cs
@@ -0,0 +1,260 @@
+function cat1() {
+  var x = document.getElementById("cat1");{
+    x.style.display = "block";
+  }
+    var x = document.getElementById("cat2");
+    x.style.display = "none";
+  }
+    var x = document.getElementById("cat3");
+    x.style.display = "none";
+  }
+    var x = document.getElementById("cat4");
+    x.style.display = "none";
+  }
+    var x = document.getElementById("cat5");
+    x.style.display = "none";
+  }
+    var x = document.getElementById("cat6");
+    x.style.display = "none";
+  }
+    var x = document.getElementById("cat7");
+    x.style.display = "none";
+  }
+    var x = document.getElementById("cat8");
+    x.style.display = "none";
+  }
+    var x = document.getElementById("cat9");
+    x.style.display = "none";
+  }
+  
+  function cat2() {
+  var x = document.getElementById("cat2");{
+    x.style.display = "block";
+  }
+    var x = document.getElementById("cat1");
+    x.style.display = "none";
+  }
+    var x = document.getElementById("cat3");
+    x.style.display = "none";
+  }
+    var x = document.getElementById("cat4");
+    x.style.display = "none";
+  }
+    var x = document.getElementById("cat5");
+    x.style.display = "none";
+  }
+    var x = document.getElementById("cat6");
+    x.style.display = "none";
+  }
+    var x = document.getElementById("cat7");
+    x.style.display = "none";
+  }
+    var x = document.getElementById("cat8");
+    x.style.display = "none";
+  }
+    var x = document.getElementById("cat9");
+    x.style.display = "none";
+  }
+  
+  function cat3() {
+  var x = document.getElementById("cat3");{
+    x.style.display = "block";
+  }
+    var x = document.getElementById("cat1");
+    x.style.display = "none";
+  }
+    var x = document.getElementById("cat2");
+    x.style.display = "none";
+  }
+    var x = document.getElementById("cat4");
+    x.style.display = "none";
+  }
+    var x = document.getElementById("cat5");
+    x.style.display = "none";
+  }
+    var x = document.getElementById("cat6");
+    x.style.display = "none";
+  }
+    var x = document.getElementById("cat7");
+    x.style.display = "none";
+  }
+    var x = document.getElementById("cat8");
+    x.style.display = "none";
+  }
+    var x = document.getElementById("cat9");
+    x.style.display = "none";
+  }
+  
+  function cat4() {
+  var x = document.getElementById("cat4");{
+    x.style.display = "block";
+  }
+    var x = document.getElementById("cat1");
+    x.style.display = "none";
+  }
+    var x = document.getElementById("cat2");
+    x.style.display = "none";
+  }
+    var x = document.getElementById("cat4");
+    x.style.display = "none";
+  }
+    var x = document.getElementById("cat5");
+    x.style.display = "none";
+  }
+    var x = document.getElementById("cat6");
+    x.style.display = "none";
+  }
+    var x = document.getElementById("cat7");
+    x.style.display = "none";
+  }
+    var x = document.getElementById("cat8");
+    x.style.display = "none";
+  }
+    var x = document.getElementById("cat9");
+    x.style.display = "none";
+  }
+  
+  function cat5() {
+  var x = document.getElementById("cat5");{
+    x.style.display = "block";
+  }
+    var x = document.getElementById("cat1");
+    x.style.display = "none";
+  }
+    var x = document.getElementById("cat3");
+    x.style.display = "none";
+  }
+    var x = document.getElementById("cat4");
+    x.style.display = "none";
+  }
+    var x = document.getElementById("cat2");
+    x.style.display = "none";
+  }
+    var x = document.getElementById("cat6");
+    x.style.display = "none";
+  }
+    var x = document.getElementById("cat7");
+    x.style.display = "none";
+  }
+    var x = document.getElementById("cat8");
+    x.style.display = "none";
+  }
+    var x = document.getElementById("cat9");
+    x.style.display = "none";
+  }
+  
+  function cat6() {
+  var x = document.getElementById("cat6");{
+    x.style.display = "block";
+  }
+    var x = document.getElementById("cat1");
+    x.style.display = "none";
+  }
+    var x = document.getElementById("cat3");
+    x.style.display = "none";
+  }
+    var x = document.getElementById("cat4");
+    x.style.display = "none";
+  }
+    var x = document.getElementById("cat5");
+    x.style.display = "none";
+  }
+    var x = document.getElementById("cat2");
+    x.style.display = "none";
+  }
+    var x = document.getElementById("cat7");
+    x.style.display = "none";
+  }
+    var x = document.getElementById("cat8");
+    x.style.display = "none";
+  }
+    var x = document.getElementById("cat9");
+    x.style.display = "none";
+  }
+  
+  function cat7() {
+  var x = document.getElementById("cat7");{
+    x.style.display = "block";
+  }
+    var x = document.getElementById("cat1");
+    x.style.display = "none";
+  }
+    var x = document.getElementById("cat3");
+    x.style.display = "none";
+  }
+    var x = document.getElementById("cat4");
+    x.style.display = "none";
+  }
+    var x = document.getElementById("cat5");
+    x.style.display = "none";
+  }
+    var x = document.getElementById("cat6");
+    x.style.display = "none";
+  }
+    var x = document.getElementById("cat2");
+    x.style.display = "none";
+  }
+    var x = document.getElementById("cat8");
+    x.style.display = "none";
+  }
+    var x = document.getElementById("cat9");
+    x.style.display = "none";
+  }
+  
+  function cat8() {
+  var x = document.getElementById("cat8");{
+    x.style.display = "block";
+  }
+    var x = document.getElementById("cat1");
+    x.style.display = "none";
+  }
+    var x = document.getElementById("cat3");
+    x.style.display = "none";
+  }
+    var x = document.getElementById("cat4");
+    x.style.display = "none";
+  }
+    var x = document.getElementById("cat5");
+    x.style.display = "none";
+  }
+    var x = document.getElementById("cat6");
+    x.style.display = "none";
+  }
+    var x = document.getElementById("cat7");
+    x.style.display = "none";
+  }
+    var x = document.getElementById("cat2");
+    x.style.display = "none";
+  }
+    var x = document.getElementById("cat9");
+    x.style.display = "none";
+  }
+  
+  function cat9() {
+  var x = document.getElementById("cat9");{
+    x.style.display = "block";
+  }
+    var x = document.getElementById("cat1");
+    x.style.display = "none";
+  }
+    var x = document.getElementById("cat3");
+    x.style.display = "none";
+  }
+    var x = document.getElementById("cat4");
+    x.style.display = "none";
+  }
+    var x = document.getElementById("cat5");
+    x.style.display = "none";
+  }
+    var x = document.getElementById("cat6");
+    x.style.display = "none";
+  }
+    var x = document.getElementById("cat7");
+    x.style.display = "none";
+  }
+    var x = document.getElementById("cat8");
+    x.style.display = "none";
+  }
+    var x = document.getElementById("cat2");
+    x.style.display = "none";
+  }
